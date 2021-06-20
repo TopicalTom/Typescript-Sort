f16f42358e400980e646ec4cfcf27fd86339502c
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-06-20 18:43:07 -0400

    Built Linked List with sorting functionality

--- build/index.js
+++ build/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
 var CharactersCollection_1 = require("./CharactersCollection");
+var LinkedList_1 = require("./LinkedList");
 // Numbers Sort
 var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
 var numberSorter = new Sorter_1.Sorter(numbersCollection);
@@ -13,3 +14,12 @@ var charactersCollection = new CharactersCollection_1.CharactersCollection('aHrH
 var characterSorter = new Sorter_1.Sorter(charactersCollection);
 characterSorter.sort();
 console.log(charactersCollection.data);
+// LinkedList Sort
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+var linkedListSorter = new Sorter_1.Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // Numbers Sort
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
@@ -13,3 +14,14 @@ const charactersCollection = new CharactersCollection('aHrHea');
 const characterSorter = new Sorter(charactersCollection);
 characterSorter.sort();
 console.log(charactersCollection.data);
+
+// LinkedList Sort
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
