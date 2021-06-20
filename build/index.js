4791cebce222a461929ba380e1a708854a2ba931
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-06-20 18:09:54 -0400

    Built Character Collection sorting class

--- build/index.js
+++ build/index.js
@@ -2,7 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
+// Numbers Sort
 var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-var sorter = new Sorter_1.Sorter(numbersCollection);
-sorter.sort();
+var numberSorter = new Sorter_1.Sorter(numbersCollection);
+numberSorter.sort();
 console.log(numbersCollection.data);
+// Characters Sort
+var charactersCollection = new CharactersCollection_1.CharactersCollection('aHrHea');
+var characterSorter = new Sorter_1.Sorter(charactersCollection);
+characterSorter.sort();
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0])
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+// Numbers Sort
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const numberSorter = new Sorter(numbersCollection);
+numberSorter.sort();
 console.log(numbersCollection.data);
+
+// Characters Sort
+const charactersCollection = new CharactersCollection('aHrHea');
+const characterSorter = new Sorter(charactersCollection);
+characterSorter.sort();
+console.log(charactersCollection.data);
